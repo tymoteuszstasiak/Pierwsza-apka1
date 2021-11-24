e293d32fe325c9ce0d187ba243d3bd7f7f0a602d
Szkola <Szkola@S24K01>
2021-11-24 15:16:27 +0100

    Dodaj pliki projektów.

---
+++ Pierwsza apka1/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pierwsza_apka1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void txtBok_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            double bok;
+            if (double.TryParse(txtBok.Text, out bok) && bok >= 0)
+            {
+                txtPole.Text = Math.Pow(bok, 2.0).ToString();
+                txtObwod.Text = (bok * 4).ToString();
+                lblKomunikat.Content = String.Empty;
+            }
+            else
+            {
+                lblKomunikat.Content = "Wpisz liczbę dodatnią";
+            }
+        }
+
+        private void btnWyczysc_Click(object sender, RoutedEventArgs e)
+        {
+            txtBok.Text = String.Empty;
+            txtObwod.Text = String.Empty;
+            txtPole.Text = String.Empty;
+            lblKomunikat.Content = "Wpisz wymiar boku";
+        }
+
+
+        
+        }
+    }
+
